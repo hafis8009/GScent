22a9048f3acc4121a2099a46e45c5c496a0c67b3
Hafiz Abdul Kareem <hafis8009@gmail.com>
2021-03-05 21:01:46 +0400

    Created a collectionview controller

---
+++ GScent/GScent/Helpers/Extensions/DashboardSectionModel+UICollectionView.swift
@@ -0,0 +1,16 @@
+//
+//  DashboardSectionModel+UICollectionView.swift
+//  GScent
+//
+//  Created by AdibUser on 05/03/2021.
+//  Copyright © 2021 Hafiz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension DashboardSectionModel {
+    func createCollectionLayoutSection() -> NSCollectionLayoutSection? {
+        return nil
+    }
+}
