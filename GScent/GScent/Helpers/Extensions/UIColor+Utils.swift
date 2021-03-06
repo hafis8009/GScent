b1a2c3b9ca1ddb1b7191ff9aaf955c55ca2e6acb
Hafiz Abdul Kareem <hafis8009@gmail.com>
2021-03-06 18:12:05 +0400

    Unit testing is done

--- GScent/GScent/Helpers/Extensions/UIColor+Utils.swift
+++ GScent/GScent/Helpers/Extensions/UIColor+Utils.swift
@@ -10,8 +10,8 @@ import UIKit
 
 extension UIColor {
     convenience init?(hex: String) {
-        let r, g, b, a: CGFloat
-        
+        let red, green, blue: CGFloat
+        var alpha: CGFloat = 1.0
         if hex.hasPrefix("#") {
             let start = hex.index(hex.startIndex, offsetBy: 1)
             let hexColor = String(hex[start...])
@@ -21,12 +21,26 @@ extension UIColor {
                 var hexNumber: UInt64 = 0
                 
                 if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    red = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    green = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    blue = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    alpha = CGFloat(hexNumber & 0x000000ff) / 255
                     
-                    self.init(red: r, green: g, blue: b, alpha: a)
+                    self.init(red: red, green: green, blue: blue, alpha: alpha)
+                    return
+                }
+            }
+            
+            if hexColor.count == 6 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    red = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    green = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    blue = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: red, green: green, blue:blue, alpha: alpha)
                     return
                 }
             }
